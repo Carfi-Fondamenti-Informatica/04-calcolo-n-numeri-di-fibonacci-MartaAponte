42288e747cd9b1776f9545567f83c732d143a1d1
MartaAponte <128090552+MartaAponte@users.noreply.github.com>
2023-04-23 09:26:18 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   return 0;
+int main(){
+    int n,primo ,a, secondo,terzo,  i=0;
+    cin>>n;
+    a = n-2;
+    primo = 1;
+    secondo = 1;
+    if (n >= 2 ){
+        cout << primo << endl;
+        cout << secondo << endl;
+        while(i< a){
+            terzo = primo + secondo ;
+            primo = secondo ;
+            secondo = terzo;
+            i++;
+            cout<< terzo<< endl;
+        }
+    }
+    else {
+        cout << "errore"<< endl;
+    }
+    return 0;
 }
